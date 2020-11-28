7693a5eec7a9b10f37ee7492a4dd10041e4c88e6
EReutov <ereutov30@gmail.com>
2020-11-28 19:35:01 +0500

    6.0.2. Заполнение массива. [#417543]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,13 +3,24 @@ package ru.job4j.array;
 public class ArrayDefinition {
 
     public static void main(String[] args) {
-        short[] ages = new short[10];
+        /*short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
 
         System.out.println("Размер массива ages: " + ages.length);
         System.out.println("Размер массива surnames: " + surnames.length);
         System.out.println("Размер массива prices: " + prices.length);
+        */
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Evgeniy Reutov";
+        names[2] = "Piter Parker";
+        names[3] = "Natalia Romanova";
+
+        for (String i : names) {
+            System.out.println(i);
+        }
 
     }
 
