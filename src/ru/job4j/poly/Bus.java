bb38e14f67ef26a91c59160fa8f6e4107ee35931
EReutov <ereutov30@gmail.com>
2020-12-05 22:58:49 +0500

    3. Интерфейс Input [#417673]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        return 0;
+    }
+}
---
+++ src/ru/job4j/poly/Transport.java
@@ -0,0 +1,11 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    void passengers(int count);
+
+    void go();
+
+    double refuel(double fuel);
+
+}
